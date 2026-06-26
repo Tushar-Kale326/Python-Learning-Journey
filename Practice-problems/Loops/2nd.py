c424c0a38a3b6d8031ec1562e92cef412f45e98f
Tushar-Kale326 <tusharkale326@gmail.com>
2026-06-26 16:17:41 +0530

    added 2nd.py of loops

---
+++ Practice-problems/Loops/2nd.py
@@ -0,0 +1,23 @@
+# Problem Statement:
+# You work in XYZ Corporation as a Data Analyst. Your company has told you to
+# work with the looping statements.
+# Tasks To Be Performed:
+# 1. Create a list that has 10, 23, 4, 26, 4, 75, 24, 54 values and with the help
+# of while loop fetch the even numbers and print the numbers.
+
+l=[10,23,4,26,4,75,24,54]
+i=0
+lengthofl=len(l)
+while i<lengthofl:
+    #num=l[i]
+    #if num%2==0:
+    #    print(num,end=" ")
+    if l[i]%2==0:
+        print(l[i],end=" ")
+        
+    i=i+1    
+    
+     
+    
+    
+    
